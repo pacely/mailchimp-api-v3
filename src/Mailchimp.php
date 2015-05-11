d3255ec51435bdc35e8f5e3a534b8cc8245bb5ad
ChrisRM <chris@hirvi.no>
2015-05-11 15:48:20 +0200

    PSR-2

--- src/Mailchimp.php
+++ src/Mailchimp.php
@@ -5,39 +5,39 @@ namespace Mailchimp;
 use GuzzleHttp\Client;
 use Illuminate\Support\Collection;
 
-class Mailchimp {
+class Mailchimp
+{
 
-	/**
-	 * Endpoint for Mailchimp API v3
-	 *
-	 * @var string
-	 */
-	private $endpoint = 'https://us1.api.mailchimp.com/3.0/';
+    /**
+     * Endpoint for Mailchimp API v3
+     *
+     * @var string
+     */
+    private $endpoint = 'https://us1.api.mailchimp.com/3.0/';
 
-	/**
-	 * @var string
-	 */
-	private $apikey;
+    /**
+     * @var string
+     */
+    private $apikey;
 
-	/**
-	 * @var Client
-	 */
-	private $client;
+    /**
+     * @var Client
+     */
+    private $client;
 
-	/**
-	 * @param string $apikey
-	 */
-	public function __construct($apikey = '')
-	{
-		$this->apikey = $apikey;
-		$this->client = new Client();
+    /**
+     * @param string $apikey
+     */
+    public function __construct($apikey = '')
+    {
+        $this->apikey = $apikey;
+        $this->client = new Client();
 
-		if (strstr($this->apikey, '-'))
-		{
-			list(, $dc) = explode('-', $this->apikey);
-			$this->endpoint = str_replace('us1', $dc, $this->endpoint);
-		}
-	}
+        if (strstr($this->apikey, '-')) {
+            list(, $dc) = explode('-', $this->apikey);
+            $this->endpoint = str_replace('us1', $dc, $this->endpoint);
+        }
+    }
 
     /**
      * @param string $method
@@ -45,14 +45,14 @@ class Mailchimp {
      * @param array $arguments
      * @return string
      */
-	public function request($resource, $arguments = [], $method = 'GET')
-	{
+    public function request($resource, $arguments = [], $method = 'GET')
+    {
         $method = strtolower($method);
 
         $resource = $this->decorateResource($resource, $arguments, $method);
 
-		return $this->call($resource, $arguments, $method);
-	}
+        return $this->call($resource, $arguments, $method);
+    }
 
     /**
      * @param $resource
@@ -60,29 +60,29 @@ class Mailchimp {
      * @param string $method
      * @return string
      */
-	private function call($resource, $arguments, $method)
-	{
-		$request = $this->client->{$method}($this->endpoint.$resource, [
-			'headers' => [
-				'Authorization' => 'apikey '.$this->apikey
-			],
-			'body' => $arguments
-		]);
+    private function call($resource, $arguments, $method)
+    {
+        $request = $this->client->{$method}($this->endpoint . $resource, [
+            'headers' => [
+                'Authorization' => 'apikey ' . $this->apikey
+            ],
+            'body'    => $arguments
+        ]);
 
         $collection = new Collection($request->json());
 
-		return $collection->collapse();
-	}
+        return $collection->collapse();
+    }
 
-	/**
-	 * Return endpoint
-	 *
-	 * @return string
-	 */
-	public function getEndpoint()
-	{
-		return $this->endpoint;
-	}
+    /**
+     * Return endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint()
+    {
+        return $this->endpoint;
+    }
 
     /**
      * @param $resource
@@ -92,9 +92,8 @@ class Mailchimp {
      */
     private function decorateResource($resource, $arguments, $method)
     {
-        if ($method == 'get')
-        {
-            $resource .= '?'.http_build_query($arguments);
+        if ($method == 'get') {
+            $resource .= '?' . http_build_query($arguments);
         }
 
         return $resource;
